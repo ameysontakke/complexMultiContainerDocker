e417b3737f1c0b76c79e8e8e6fe3d2ef1885d9ac
ameysontakke <sontakkeamey@gmail.com>
2020-07-12 10:06:51 +0000

    corrected parenthesis for App.test.js file

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {};
+it('renders without crashing', () => {});
 
 /*below commented code can be used for developement run only.*/
 /*it('renders without crashing', () => {
